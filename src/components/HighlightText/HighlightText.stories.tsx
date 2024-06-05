1fd693f8158c8e935ce13af7a461ed0992d7da0a
huy.le3 <huy.le3@mservice.com.vn>
2024-06-05 13:09:52 +0700

    add text

--- src/components/HighlightText/HighlightText.stories.tsx
+++ src/components/HighlightText/HighlightText.stories.tsx
@@ -13,7 +13,17 @@ type Story = StoryObj<typeof HighlightText>;
 export const Primary: Story = {
   render: () => (
     <div style={{ marginTop: 80, marginLeft: 40 }}>
-      <HighlightText colors={["red", "green", "blue"]}>123456789</HighlightText>
+      <HighlightText colors={["red", "green", "blue"]}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </HighlightText>
     </div>
   ),
 };
@@ -22,7 +32,15 @@ export const Secondary: Story = {
   render: () => (
     <div style={{ marginTop: 80, marginLeft: 40 }}>
       <HighlightText colors={["#ff00ff", "#ffff00", "#00ff00"]}>
-        123456789
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
       </HighlightText>
     </div>
   ),
